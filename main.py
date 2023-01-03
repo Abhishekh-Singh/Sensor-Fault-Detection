e00cad04af78652302c6d7fde020c33663568369
Abhishekh-Singh <abhisheksinghjin@gmail.com>
2023-01-04 01:37:29 +0530

    added training and data-ingestion pipeline configs

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 
-# if __name__ == '__main__':
-#     mongodb_client = MongoDBClient()
+if __name__ == '__main__':
+    # mongodb_client = MongoDBClient()
 
-#     print("Collection names:",mongodb_client.database.list_collection_names())
+    # print("Collection names:",mongodb_client.database.list_collection_names())
+
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
